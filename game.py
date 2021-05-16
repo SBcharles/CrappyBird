da89ab25c09ee0b851cd64faa7deacea438606dd
SBcharles <sebburgess96@gmail.com>
2021-05-16 17:04:09 +0100

    stationary bird added to screen centre

--- game.py
+++ game.py
@@ -11,11 +11,15 @@ clock = pygame.time.Clock()
 background = pygame.image.load("assets/background-day.png")
 background_rect = background.get_rect()
 
+bird = pygame.image.load("assets/yellowbird-midflap.png")
+bird_rect = bird.get_rect(center=(144, 256))
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
 
     screen.blit(background, background_rect)
+    screen.blit(bird, bird_rect)
     pygame.display.flip()
     clock.tick(60)
