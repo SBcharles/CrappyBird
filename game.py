851394f47210ddd990989f8b87a2a511b2fed865
SBcharles <sebburgess96@gmail.com>
2021-05-16 17:20:33 +0100

    One floor made to move

--- game.py
+++ game.py
@@ -3,24 +3,30 @@ import pygame
 
 pygame.init()
 
-size = width, height = 288, 512
 
-screen = pygame.display.set_mode(size)
+class Config:
+    window_size = window_width, window_height = 288, 512
+    scroll_speed = -1
+
+
+screen = pygame.display.set_mode(Config.window_size)
 clock = pygame.time.Clock()
 
 background = pygame.image.load("assets/background-day.png")
 background_rect = background.get_rect()
 
 bird = pygame.image.load("assets/yellowbird-midflap.png")
-bird_rect = bird.get_rect(center=(width / 2, height / 2))
+bird_rect = bird.get_rect(center=(Config.window_width / 2, Config.window_height / 2))
 
 floor = pygame.image.load("assets/base.png")
-floor_rect = floor.get_rect(bottomleft=(0, height))
+floor_rect = floor.get_rect(bottomleft=(0, Config.window_height))
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+    floor_rect = floor_rect.move(Config.scroll_speed, 0)
 
     screen.blit(background, background_rect)
     screen.blit(bird, bird_rect)
