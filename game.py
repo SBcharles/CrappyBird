a3ecc402eeba43623431c2bf550756d2942c3d7b
SBcharles <sebburgess96@gmail.com>
2021-05-16 17:10:02 +0100

    stationary floor added to the bottom

--- game.py
+++ game.py
@@ -12,7 +12,10 @@ background = pygame.image.load("assets/background-day.png")
 background_rect = background.get_rect()
 
 bird = pygame.image.load("assets/yellowbird-midflap.png")
-bird_rect = bird.get_rect(center=(144, 256))
+bird_rect = bird.get_rect(center=(width / 2, height / 2))
+
+floor = pygame.image.load("assets/base.png")
+floor_rect = floor.get_rect(bottomleft=(0, height))
 
 while 1:
     for event in pygame.event.get():
@@ -21,5 +24,6 @@ while 1:
 
     screen.blit(background, background_rect)
     screen.blit(bird, bird_rect)
+    screen.blit(floor, floor_rect)
     pygame.display.flip()
     clock.tick(60)
